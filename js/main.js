b1ab63e6abed8e16df73efc8d3caf31725e58887
Mostafa-Ashraff <94311378+Mostafa-Ashraff@users.noreply.github.com>
2022-12-04 19:25:04 +0200

    preloader

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+// var bg = document.body.style.backgroundImage;
+
+// gsap.to(document.body.style.backgroundImage,{rotation:360 , duration:2})
+
+gsap.timeline()
+  .from(".logo", {opacity:0, scale:0, ease:"back" ,duration:3})
+
+gsap.to('.loading-bg', {
+  rotate: 360, 
+  duration: 60,
+  ease: 'linear',
+  repeat:-1
+})
+  // var loader =document.getElementById("preloader");
+  // window.addEventListener("load" ,function(){
+  //   loader.style.display="none";
+  // })
+
+  setTimeout(function(){
+    document.querySelector('.preloader').style.display = 'none';
+    //$(".preloader").fadeToggle();
+    $(".head").removeClass("d-none").addClass("d-block");
+},3500);
